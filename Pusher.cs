98bad0c1e9969c20e6d76f2c661df9af59bccebb
joexi <407154812@qq.com>
2013-12-09 11:30:44 +0800

    Update Pusher.cs

--- Pusher.cs
+++ Pusher.cs
@@ -6,14 +6,18 @@ public class Pusher : MonoBehaviour {
     private static GameObject Host = new GameObject();
     private static GameObject LockObj = new GameObject();
     private static List<Action> actions = new List<Action>();
+
     public static void Instantiate()
     {
-        Pusher pusher = Host.AddComponent<Pusher>();
-        Host.name = "Global_Pusher";
-        LockObj.name = "Lock";
-        LockObj.transform.parent = Host.transform;
-        DontDestroyOnLoad(Host);
-        DontDestroyOnLoad(LockObj);
+        if (Host.GetComponent<Pusher>() == null)
+        {
+            Pusher pusher = Host.AddComponent<Pusher>();
+            Host.name = "Global_Pusher";
+            LockObj.name = "Lock";
+            LockObj.transform.parent = Host.transform;
+            DontDestroyOnLoad(Host);
+            DontDestroyOnLoad(LockObj);
+        }
     }
 
 	void Start () {
